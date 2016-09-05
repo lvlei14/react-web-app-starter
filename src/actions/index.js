49054d6aae93e290815be64e988ab63ee95d967d
chenkaiC4 <chenkaic4@gmail.com>
2016-09-06 00:07:19 +0800

    register redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,14 @@
 import * as todo from './todo';
 import * as globalAlert from './global_alert';
+import * as register from './register';
+
 
 export const todoActions = todo;
 export const globalAlertActions = globalAlert;
+export const registerActions = register;
 
 export default {
   todoActions,
   globalAlertActions,
+  registerActions,
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import todo from './todo';
 import globalAlert from './global_alert';
+import register from './register';
 
 const rootReducer = combineReducers({
   todo,
-  globalAlert
+  globalAlert,
+  register
 })
 
 export default rootReducer;
